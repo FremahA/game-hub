9af8f3b276b3c5214133781e6315ec7166a3f586
Fremah Agyemang <captainfremah66@gmail.com>
2023-08-22 01:40:35 +0000

    Build a color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <Grid
@@ -12,11 +13,11 @@ function App() {
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>
