26d3b4e974f014da54140377d25e40e3232fe909
Fremah Agyemang <captainfremah66@gmail.com>
2023-08-22 22:18:20 +0000

    Create a custom hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "7416d3d356a74362aceb8df3785663c0",
+    key: "4d8af297708e4686ab6b19040cb42b81",
   },
 });
