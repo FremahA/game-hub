a68d665d6fa4ed43cc4868bbf7f2e732fde26a9c
Fremah Agyemang <captainfremah66@gmail.com>
2023-08-28 15:29:44 +0000

    Ship genres with the app.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,12 @@
 import {
   Button,
-  HStack,
   Heading,
+  HStack,
   Image,
   List,
   ListItem,
   Spinner,
+  Text,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -17,11 +18,14 @@ interface Props {
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
+
   if (error) return null;
-  if (isLoading) return <Spinner></Spinner>;
+
+  if (isLoading) return <Spinner />;
+
   return (
     <>
-      <Heading fontSize="2xl" marginBottom={3}>
+      <Heading fontSize="2xl" marginTop={9} marginBottom={3}>
         Genres
       </Heading>
       <List>
@@ -33,13 +37,13 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
                 borderRadius={8}
                 objectFit="cover"
                 src={getCroppedImageUrl(genre.image_background)}
-              ></Image>
+              />
               <Button
                 whiteSpace="normal"
                 textAlign="left"
                 fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
-                fontSize="lg"
+                fontSize="md"
                 variant="link"
               >
                 {genre.name}
